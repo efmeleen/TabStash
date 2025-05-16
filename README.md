72b9780cbe08d3bf0ad5cba5d97afab24fa76f7f
efmeleen <ethanfmeleen@gmail.com>
2025-05-16 14:23:29 -0400

    Implemented stash size badge on browser action. Updated README

--- README.md
+++ README.md
@@ -1,23 +1,27 @@
 # TabStash
 
+## Overview
+Have you ever kept a tab open because you were worried that you might never come back to it if you saved it to your bookmarks?
+
+The goal of this extension is to allow you to close those tabs with the confidence that you will eventually come back to them. It accomplishes this by allowing you to "stash" tabs in a queue (stored in your bookmarks). You can retrieve tabs from this queue later.
+
 ## What it does
+TabStash displays a button in the menu bar with three functions:
+1. Left-clicking on the button causes a popup menu to open (no functionality currently)
+2. Ctrl+Left-clicking on the button puts saves the tab to the back of the queue and closes the tab
+3. Ctrl+Shift+Left-clicking on the button opens a new tab, navigates to the URL of the tab at the front of the queue, and then removes that tab from the queue
 
-Displays a button in the menu bar with three functions:
-1. Left-clicking on the button causes a popup menu to open
-2. Ctrl+Left-clicking on the button bookmarks the current tab in a bookmarks folder called TabStash and closes the tab
-3. Ctrl+Shift+Left-clicking on the button opens a new tab, sets that tab's URL to the URL of the oldest bookmark in the TabStash folder, then deletes that bookmark
+The extension stores its queue in the browsers bookmarks to prevent any issues that could arise if the extension stops working or is removed. This also allows your queue to be synchronized between browsers by the same means the rest of your bookmarks are synchronized.
 
-In effect, this creates a queue data structure which tabs can be added to and removed from.
-
-The intent is to preserve any tab that I don't currently want to deal with in a way that guarentees that I will come back to it before revisiting any tab that was stashed afterwards.
+The intended use of this extension is to stash any tab that you don't want to deal with immediately in a way that guarantees that you will revisit it before revisiting any tab that was stashed afterwards.
 
 ## Plans
-Currently the basic stashing and retrieving functionality works as desired.  Plans will be divided into tasks that need to be complete before releasing this extension, and tasks that can be completed after releasing the extension.
+Currently the basic stashing and retrieving functionality works as desired.  Plans will be divided into tasks that need to be completed before releasing this extension, and tasks that can be completed after releasing the extension.
 ### Pre-Release
 - Make the brower action's popup useful in some way, or remove it.
 - Add error handling so that no unexpected behavior occurs when Promises fail, or if the bookmark folder is empty or removed, etc.
-- Display the number of stashed tabs on the browser action.
+- New Icons
 ### Post-Release
 - Add a settings page
 - Allow users to change the shortcuts used to stash and retrieve tabs
-- Implement shortcuts to stash and retrieve tabs that rely only on the keyboard (no clicking on the browser action)
+- Allow shortcuts that rely only on the keyboard (no clicking on the browser action needed)
--- background.js
+++ background.js
@@ -1,28 +1,43 @@
-let bookmarkFolderId;
+let bookmarkFolderId = undefined;
 
 // Get the extension's bookmark folder id. If it doesn't exist, create it
 browser.bookmarks.search({title: "TabStash", url: undefined}).then((results) => {
     if(results.length == 0){
-        // create it, then assign it to bookmarkFolder
-        console.log("CREATING FOLDER")
-        browser.bookmarks.create({title: "TabStash", url: undefined}).then((node) => 
-            {
-                bookmarkFolderId = node.id
-                console.log(bookmarkFolderId)
-            }
-        )
+        // create it, then assign its ID to bookmarkFolderId
+        browser.bookmarks.create({title: "TabStash", url: undefined}).then((node) => {bookmarkFolderId = node.id}).then(updateBrowserActionStashSize)
     }else if(results.length == 1){
         // assign it to bookmarkFolder for easy access
         console.log("FOUND FOLDER")
         bookmarkFolderId = results[0].id
         console.log(bookmarkFolderId)
+        updateBrowserActionStashSize()
     }else{
         //something has gone wrong
         console.log("THERE ARE MULTIPLE FOLDERS")
     }
 }) //TODO: maybe a failure in this can appear in the extension's popup "failed to find or create bookmark folder"
 
-/* Bookmark the current tab
+// TODO: call this after the folder is created/found to initialize the badge
+// TODO: Maybe display the actual number in the popup? In the case of 99+ this could be useful
+// Sets the badge text of the browser action to the size of the stash
+function updateBrowserActionStashSize(){
+    if(bookmarkFolderId != undefined){
+        browser.bookmarks.getSubTree(bookmarkFolderId).then((tree) => {
+            if(tree.length == 1){
+                if(tree[0].children.length == 0){
+                    browser.browserAction.setBadgeText({text: ""})
+                }else if(tree.length > 99){
+                    browser.browserAction.setBadgeText({text: "99+"})
+                }else{
+                    browser.browserAction.setBadgeText({text: String(tree[0].children.length)})
+                }
+            }
+        })
+    }
+}
+
+/* 
+ * Bookmark the current tab
  * If the bookmarking was successful, close the tab
  */
 function stashCurrentTab(){
@@ -32,18 +47,13 @@ function stashCurrentTab(){
     // Bookmark the current tab, then close it if the bookmark was created successfully
     querying.then((tabs) => {
         if(tabs.length > 0){
-            // browser.bookmarks.create({title: tabs[0].title, url: tabs[0].url}).then(() => 
-            //     {
-            //         browser.tabs.remove(tabs[0].id)
-            //     }
-            // );
             browser.bookmarks.create({
                 parentId: bookmarkFolderId,
                 title: tabs[0].title,
                 url: tabs[0].url
             }).then(() => 
                 {
-                    browser.tabs.remove(tabs[0].id)
+                    browser.tabs.remove(tabs[0].id).then(updateBrowserActionStashSize)
                 }
             );
             //TODO: Add fail condition here in case extension's folder has been deleted
@@ -57,6 +67,7 @@ function stashCurrentTab(){
  * Create a new tab with that bookmark's URL
  * If tab creation was successful, delete the bookmark
  */
+//TODO: what if the stash is empty?
 function retrieveOldestTab(){
     browser.bookmarks.getSubTree(bookmarkFolderId).then((result) => 
     {
@@ -64,15 +75,15 @@ function retrieveOldestTab(){
             const oldestBookmark = result[0].children.reduce((oldestChildFound, currentChild) => {
                 return currentChild.dateAdded < oldestChildFound.dateAdded ? currentChild : oldestChildFound;
             })
-            console.log(result[0])
-            console.log(oldestBookmark)
+            // console.log(result[0])
+            // console.log(oldestBookmark)
             //TODO: Handle if there are no bookmarks in the folder
 
             browser.tabs.create({
                 active: true,
                 url: oldestBookmark.url
             }).then(() => {
-                browser.bookmarks.remove(oldestBookmark.id)
+                browser.bookmarks.remove(oldestBookmark.id).then(updateBrowserActionStashSize)
             })
         }else{
             //TODO: something has gone wrong, the folder is gone?
@@ -81,8 +92,7 @@ function retrieveOldestTab(){
 }
 
 /* 
- * Show the popup if the browser action is clicked on with no other key pressed
- * Do something else if shift or control is held when the browser action is clicked
+ * If browser action is Shift+Ctrl+clicked, retrieve the oldest tab in the stash 
  */
  function browserActionClickHandler(tab, data){
     if(data.modifiers.includes("Shift") && data.modifiers.includes("Ctrl")){
@@ -90,9 +100,10 @@ function retrieveOldestTab(){
     }else if(data.modifiers.includes("Ctrl")){
         stashCurrentTab()
     }else{
-        browser.browserAction.setPopup({ popup: "popup/popup.html"});
+        // Need to do it this way because the browserActionClick event won't fire if the browser action has a popup
+        browser.browserAction.setPopup({ popup: "popup/popup.html" });
         browser.browserAction.openPopup();
-        browser.browserAction.setPopup({ popup: null});
+        browser.browserAction.setPopup({ popup: null });
     }
 }
 
